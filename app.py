6a4cffb848255cba3301af949b3eb68f377aa243
emiberndt <emi.berndt@gmail.com>
2021-05-10 19:49:17 +0000

    Fuctionality for registrating users

--- app.py
+++ app.py
@@ -27,6 +27,22 @@ def get_reviews():
 
 @app.route("/register", methods=["GET", "POST"])
 def register():
+    if request.method == "POST":
+        existing_user = mongo.db.user.find_one(
+            {"username": request.form.get("username").lower()})
+
+        if existing_user:
+            flash("This username is already taken")
+            return redirect(url_for("register"))
+
+        register = {
+            "username": request.form.get("username").lower(),
+            "password": generate_password_hash(request.form.get("password"))
+        }
+        mongo.db.user.insert_one(register)
+
+        session["users"] = request.form.get("username").lower()
+        flash("Registration Completed!")
     return render_template("register.html")
 
 
